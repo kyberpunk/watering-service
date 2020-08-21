53f7da09599ea9f17a55f309cb7e649e7aac9abb
Vít Holásek <xvh@seznam.cz>
2020-08-21 14:01:56 +0200

    Initial commit

---
+++ src/main/java/com/kyberpunk/iot/watering/WateringApplication.java
@@ -0,0 +1,13 @@
+package com.kyberpunk.iot.watering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WateringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WateringApplication.class, args);
+	}
+
+}
