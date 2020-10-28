fc0dd8c4aa336c612496531545a27ec279bb2aa8
Holasek, Vit <vit.holasek@siemens.com>
2020-10-28 19:40:22 +0100

    Use pretty time formating and enhance localizations.

--- src/main/java/com/kyberpunk/iot/watering/dto/DateFormatHelper.java
+++ src/main/java/com/kyberpunk/iot/watering/dto/DateFormatHelper.java
@@ -1,8 +1,11 @@
 package com.kyberpunk.iot.watering.dto;
 
+import org.ocpsoft.prettytime.PrettyTime;
 import org.springframework.context.i18n.LocaleContextHolder;
 
+import java.util.Date;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
@@ -12,5 +15,13 @@ public class DateFormatHelper {
             return "";
         return localDateTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
                 .withLocale(LocaleContextHolder.getLocale()));
+    }
+
+    public static String pretty(LocalDateTime localDateTime) {
+        if (localDateTime == null)
+            return "";
+        PrettyTime pretty = new PrettyTime(LocaleContextHolder.getLocale());
+        Date date = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+        return pretty.format(date);
     }
 }
